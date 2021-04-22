372b8a7580c23f98d891a8929a68a8ba0d8e1087
omranimed <mohamed.omrani@esprit.tn>
2021-04-22 02:18:00 +0100

    push

---
+++ artraction/src/Form/UserType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('email')
+            ->add('password')
+            ->add('numero')
+            ->add('age')
+            ->add('image')
+            ->add('addresse')
+            ->add('role', ChoiceType::class, [
+                'choices'  => [
+                    'Actriste' => 'Artiste',
+                    'Client' => 'Client',
+
+                ],
+            ]);
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
